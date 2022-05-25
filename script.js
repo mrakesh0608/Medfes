189114929a734e7f4d128a02db93bae25c5d1067
mrakesh0608 <mrakesh0608@gmail.com>
2022-05-25 12:04:12 +0530

    added fun

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('toggle-register-btn').addEventListener('click',()=>toggle_form(1))
+document.getElementById('toggle-login-btn').addEventListener('click',()=>toggle_form(0))
+
+
+function toggle_form(val)
+{
+    if(val)
+    {
+        document.getElementById('Sign-In').style.display = 'none';
+        document.getElementById('Register').style.display = 'block';
+    }
+    else
+    {  
+        document.getElementById('Register').style.display = 'none';
+        document.getElementById('Sign-In').style.display = 'block'
+    }
+}
