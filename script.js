2b327b202b1adb8897b1ee456063d242e2580977
mrakesh0608 <mrakesh0608@gmail.com>
2022-05-25 16:55:17 +0530

    imporved css

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.getElementById('toggle-register-btn').addEventListener('click',()=>toggle_form(1));
 document.getElementById('toggle-login-btn').addEventListener('click',()=>toggle_form(0));
 
-function toggle_form(val)
-{
-    if(val)
-    {
+function toggle_form(val){
+    if(val){
         document.getElementById('Sign-In').style.display = 'none';
         document.getElementById('Register').style.display = 'block';
-    }
-    else
-    {  
+    }else{  
         document.getElementById('Register').style.display = 'none';
         document.getElementById('Sign-In').style.display = 'block'
     }
 }
+
+const label = document.getElementsByClassName('form__label');
+Array.from(label).forEach(element =>{
+    element.addEventListener('click',(e)=>{
+        element.previousElementSibling.focus();
+    })
+});
